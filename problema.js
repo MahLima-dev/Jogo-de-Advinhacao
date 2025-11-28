1dd9a92548d1eb0bb46b391172935f551acd7ca7
Matheus <matheuslima_201118@hotmail.com>
2025-11-28 09:36:19 -0300

    Atualização

--- problema.js
+++ problema.js
@@ -13,7 +13,7 @@ while (tentativas < 10) {
         document.getElementById("mensagem").textContent = "Digite um valor para começar";
         break;
     }
-    if (isNaN(palpite) || palpite < 0 || palpite > 100) {
+    if (isNaN(palpite) || palpite < 1 || palpite > 100) {
         document.getElementById("mensagem").textContent = "Insira um número válido, e entre 1 e 100.";
         break;
     }
@@ -35,10 +35,10 @@ while (tentativas < 10) {
     
     }
 }
-    if (tentativas === 8) {
+    if (!jogoFinalizado && tentativas === 8) {
         document.getElementById("mensagem").textContent = "Dica: o número está entre " + parseInt((numeroAleatorio - 8)) + " e " + parseInt((numeroAleatorio + 15));
     }
-    if (tentativas === 10) {
+    if (!jogoFinalizado && tentativas === 10) {
         document.getElementById("tentativas").textContent = "";
         document.getElementById("mensagem").textContent = "Game Over! O número era " + numeroAleatorio;
         jogoFinalizado = true
@@ -47,7 +47,8 @@ while (tentativas < 10) {
 function resetar(){
     numeroAleatorio = Math.floor(Math.random() *100)+1;
     tentativas = 0
-    document.getElementById("palpite").textContent = ""
+    jogoFinalizado = false
+    document.getElementById("palpite").value = ""
     document.getElementById("mensagem").textContent = ""
     document.getElementById("tentativas").textContent = ""
 }
